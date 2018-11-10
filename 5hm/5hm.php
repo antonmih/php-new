7c6eece0a7febdabb27dff8e275eb8ffb12a20f8
Anton <antonmih>
2018-11-10 19:35:32 +0700

    4 hm

--- 5hm/5hm.php
+++ 5hm/5hm.php
@@ -4,6 +4,43 @@
 </head>
 <body>
 <h1 style="text-align: center">5 домашняя работа</h1>
+<?php
 
+class GuestBook
+{
+    public $file;
+    public $guest;
+    public $text;
+
+    public function __construct($file)
+    {
+        $this->guest = file(__DIR__.'/'.$this->file = $file);
+    }
+
+    public function getData()
+    {
+        return $this->guest;
+    }
+
+    public function append($text)
+    {
+        $this->guest[] = "\n".$this->text = $text;
+    }
+
+    public function save()
+    {
+        file_put_contents(__DIR__.'/'.$this->file, $this->guest);
+    }
+}
+
+    $guest = new GuestBook('db.txt');
+    $guest->append('Anna');
+    $guest->save();
+    var_dump($guest->getData());
+
+
+
+
+?>
 </body>
 </html>
